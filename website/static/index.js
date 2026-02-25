860f66c21a040a1c05c1fe95c457b9c334e0a558
czh6591-arch <czh6591@gmail.com>
2026-02-26 03:32:52 +1100

    glm-5

--- website/static/index.js
+++ website/static/index.js
@@ -6,3 +6,32 @@ function DeleteNote(noteid){
     window.location.href = "/";
   });
 }
+
+function SaveNote(noteid){
+  var noteItem = document.querySelector(`li[data-note-id="${noteid}"]`);
+  var title = noteItem.querySelector('.note-title-input').value;
+  var content = noteItem.querySelector('.note-content-input').value;
+  
+  fetch("/update-note", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ 
+      noteid: noteid,
+      title: title,
+      content: content
+    }),
+  })
+  .then(response => response.json())
+  .then(data => {
+    if(data.success){
+      alert('笔记保存成功！');
+    } else {
+      alert('保存失败：' + data.message);
+    }
+  })
+  .catch(error => {
+    alert('保存出错：' + error);
+  });
+}
